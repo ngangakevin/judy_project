3d057c69d8758043950df0e24268f82bd446286b
ngangakevin <ngangakelvin254@gmail.com>
2019-03-19 00:28:22 +0300

    second commit

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+			<p>In the recent past many of the places you look up to for 
+			fashion sense have not been considering 
+			the latest trends in the fashion industry. Sonn was fonded to
+			 fill the void left by it's predecessors providing 
+			the latest trends, fashion and designs for people with great taste.
+			 Our personell are characterised with a deep sence of passsion 
+			 for sytle to satisfy your unique preferences thus making us
+			  the go-to choice when shopping for your clothing.
+			 <a href="login.php">Welcome to Sonn Elegance!</a></p>
+			</div>
+	</div>
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/scripts.php'; ?>
+</body>
+
---
+++ contacts.php
@@ -0,0 +1,29 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+			</div>
+<div>
+	<p>You can contact us through:</p>
+	<p>email<a href="mailto:judygathiru1@gmail.com"> judygathiru1@gmail.com</a></p>
+	<p>our official number <strong>0797363722</strong></p>
+	<p>or visit at any social media platforms under our name 'Sonn Elegance'</p>
+</div>
+	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+	    	<a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
+	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+</div>
+
+	</div>
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/scripts.php'; ?>
+</body>
+
